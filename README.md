ceb2a768ab35c08025c70745f6e14d698bc6a4a6
Alejandro Exequiel Hernández Lara <alejandrehl@icloud.com>
2021-08-30 17:13:41 -0400

    Mostrando circular progress indicator cuando la webview esta cargando

--- README.md
+++ README.md
@@ -4,3 +4,7 @@
 
 1. [Webview Flutter](https://pub.dev/packages/webview_flutter)
 2. [Device Info](https://pub.dev/packages/device_info)
+
+### Links
+
+1. [Circular progress indicator when webview loads](https://stackoverflow.com/questions/58713912/how-to-show-loading-indicator-in-webview-flutter)
--- lib/widgets/main_webview.dart
+++ lib/widgets/main_webview.dart
@@ -16,6 +16,8 @@ class _MainWebviewState extends State<MainWebview> {
   final Completer<WebViewController> _controller =
       Completer<WebViewController>();
 
+  bool isLoading = true;
+
   @override
   void initState() {
     super.initState();
@@ -62,33 +64,41 @@ class _MainWebviewState extends State<MainWebview> {
 
     return Builder(
       builder: (BuildContext context) {
-        return WebView(
-          initialUrl: url,
-          javascriptMode: JavascriptMode.unrestricted,
-          onWebViewCreated: (WebViewController webViewController) {
-            _controller.complete(webViewController);
-          },
-          onProgress: (int progress) {
-            print("WebView is loading (progress : $progress%)");
-          },
-          javascriptChannels: <JavascriptChannel>{},
-          navigationDelegate: (NavigationRequest request) {
-            if (request.url.startsWith('https://www.youtube.com/')) {
-              print('blocking navigation to $request}');
-              return NavigationDecision.prevent;
-            }
+        return Stack(
+          children: <Widget>[
+            WebView(
+              initialUrl: url,
+              javascriptMode: JavascriptMode.unrestricted,
+              onWebViewCreated: (WebViewController webViewController) {
+                _controller.complete(webViewController);
+              },
+              onProgress: (int progress) {
+                print("WebView is loading (progress : $progress%)");
+              },
+              javascriptChannels: <JavascriptChannel>{},
+              navigationDelegate: (NavigationRequest request) {
+                if (request.url.startsWith('https://www.youtube.com/')) {
+                  print('blocking navigation to $request}');
+                  return NavigationDecision.prevent;
+                }
 
-            print('allowing navigation to $request');
-            return NavigationDecision.navigate;
-          },
-          onPageStarted: (String url) {
-            print('Page started loading: $url');
-          },
-          onPageFinished: (String url) {
-            print('Page finished loading: $url');
-            getDeviceInfo();
-          },
-          gestureNavigationEnabled: true,
+                print('allowing navigation to $request');
+                return NavigationDecision.navigate;
+              },
+              onPageStarted: (String url) {
+                print('Page started loading: $url');
+                getDeviceInfo();
+              },
+              onPageFinished: (String url) {
+                print('Page finished loading: $url');
+                setState(() {
+                  isLoading = false;
+                });
+              },
+              gestureNavigationEnabled: true,
+            ),
+            isLoading ? Center(child: CircularProgressIndicator()) : Stack()
+          ],
         );
       },
     );
